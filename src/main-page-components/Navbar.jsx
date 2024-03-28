971747a25d5c145af8ad70131e2955306f7310d3
Kavilash-R <kavilashravi@gmail.com>
2024-03-28 22:34:43 +0530

    updated

--- src/main-page-components/Navbar.jsx
+++ src/main-page-components/Navbar.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div></div>
+      <div className="w-100% h-20 bg-slate-500 flex">
+        <h1 className="text-3xl font-bold items-center">CAKES OF PARADISE</h1>
+        <a href=""></a>
+        <button></button>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
     </>
   );
 };
