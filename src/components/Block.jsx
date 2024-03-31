af4b0ec36cc68d3493c4b91a0c39edbca5063cd8
Kavilash-R <kavilashravi@gmail.com>
2024-03-31 22:26:15 +0530

    updated

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Block = () => {
   return (
     <>
-      <div className="w-60 h-120 m-10 mr-6 border-b-8 bg-gray-200">
-        <div>
-          <img className="w-48 ml-6 h-48" src="" alt="" />
-          <p className="w-100% text-center my-2">hello</p>
-          <p className="w-100% text-center my-2">price</p>
+      <div className="w-60 h-[360px]  m-10 mr-6  ">
+        <div className="border rounded-xl border-gray-700 bg-gray-200">
+          <img className="w-48 ml-6 h-48" src="" alt="cakes" />
+          <p className="w-100% text-center my-2 ">hello</p>
+          <p className="w-100% text-center my-2">Price:</p>
         </div>
-        <div>
+        <div className="mt-5">
           <center>
-            <button className="text-center my-2 ">Cart</button> <br />
-            <button className="text-center my-2 ">Purchase</button>
+            <button className="text-center py-1 bg-yellow-400 border  border-gray-800 rounded-lg w-[100%]  ">
+              Add to Cart
+            </button>
+            <br />
+            <br />
+            <button className="text-center py-1 bg-orange-400 border  border-gray-800 rounded-lg w-[100%] ">
+              Purchase
+            </button>
           </center>
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Main = () => {
   return (
@@ -8,7 +9,7 @@ const Main = () => {
         <div className="py-8 ">
           <h1 className="min-[0px]:text-2xl md:text-3xl lg:text-5xl ">
             GREETINGS!!
-          </h1>{" "}
+          </h1>
           <br />
           <p className="min-[0px]:text-2xl md:text-3xl lg:text-4xl">
             Welcome, where every bite is a taste of happiness!
@@ -26,33 +27,71 @@ const Main = () => {
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3   min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3  
              min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5   sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] lg:m-10 bg-gray-100"
           >
-            <div className="h-[30%] w-[30%] border mr-[50%] mt-[8%]"></div>
+            <img
+              src="./images/cakes-2.png"
+              className="h-[30%] w-[30%] rounded-lg mr-[50%] mt-[8%]  "
+            ></img>
+            <br />
+            <h1 className="text-start font-bold ml-7 ">CAKES</h1>
+            <h3 className="sm:block text-center min-[0px]:hidden">
+              Crafted with love and passion!
+            </h3>
+            <br />
+            <p className=" border mt-2 border-gray-500 bg-white rounded-3xl flex items-center p-1 w-[50%] text-center">
+              &nbsp;&nbsp; Explore &nbsp; <FaArrowRightLong />
+            </p>
           </Link>
+
           <Link
             to="/puff"
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3  
              min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3 min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5 sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] bg-gray-100"
-          ></Link>
+          >
+            <img
+              src="./images/puffs-set.jpg"
+              className="h-[30%] w-[30%] rounded-lg mr-[50%] mt-[8%]  "
+            ></img>
+          </Link>
           <Link
             to="/bread"
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3 
               min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3   min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5   sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] bg-gray-100"
-          ></Link>
+          >
+            <img
+              src="./images/bread.png"
+              className="h-[30%] w-[30%] border bg-white rounded-lg mr-[50%] mt-[8%]  "
+            ></img>
+          </Link>
           <Link
             to="/chat"
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3 
               min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3   min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5   sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] bg-gray-100"
-          ></Link>
+          >
+            <img
+              src="./images/chat-items.jpg"
+              className="h-[30%] w-[30%] rounded-lg mr-[50%] mt-[8%]  "
+            ></img>
+          </Link>
           <Link
             to="/cookies"
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3 
             min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3   min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5   sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] bg-gray-100"
-          ></Link>
+          >
+            <img
+              src="./images/cookies-set.png"
+              className="h-[30%] w-[30%] rounded-lg mr-[50%] mt-[8%]  bg-white "
+            ></img>
+          </Link>
           <Link
             to="/others"
             className="border rounded-3xl hover:border-gray-800  min-[0px]:h-40 min-[0px]:w-40 min-[0px]:m-3 
               min-[303px]:h-32 min-[303px]:w-32 min-[303px]:m-3   min-[400px]:h-60 min-[400px]:w-60 min-[400px]:m-5   sm:h-60 sm:w-60 sm:m-10  lg:h-[300px] lg:w-[300px] bg-gray-100"
-          ></Link>
+          >
+            <img
+              src="./images/others.png"
+              className="h-[30%] w-[30%] rounded-lg mr-[50%] mt-[8%] bg-white "
+            ></img>
+          </Link>
         </div>
       </center>
     </>
